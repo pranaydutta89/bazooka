476b1169738b284f42c4b22dc4af7b15123e6afb
pranay dutta <pranaydutta89@gmail.com>
2019-08-26 13:10:57 +0530

    changes

--- app/components/header/header.component.js
+++ app/components/header/header.component.js
@@ -3,20 +3,31 @@ import routes from '../routes';
 class Header extends LitElement {
 
 
+  static get properties() {
+    return {
+      currentRoute: { type: String }
+    }
+  }
+
   constructor() {
     super();
+    this.currentRoute = '/home';
   }
+
 
   createRenderRoot() {
     return this;
   }
+
   linkClick(event, route) {
     event.preventDefault();
+    this.currentRoute = route;
     routes.navigate(route);
   }
 
   render() {
     return html`
+    <css-ele></css-ele>
     <style>
       .heading{
          margin-bottom:0.5rem
@@ -24,20 +35,20 @@ class Header extends LitElement {
     </style>
     <div class="heading">
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="/" @click='${(evt) => this.linkClick(evt, '/')}'>{ Bazooka }</a>
+  <a class="navbar-brand" href="/home" @click='${(evt) => this.linkClick(evt, '/home')}'>{ Bazooka }</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/" @click='${(evt) => this.linkClick(evt, '/')}'>Home</a>
+      <li class="nav-item ${this.currentRoute === '/home' ? 'active' : 'empty'}">
+        <a class="nav-link" href="/home" @click='${(evt) => this.linkClick(evt, '/home')}'>Home</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item ${this.currentRoute === '/games' ? 'active' : 'empty'}">
         <a class="nav-link" href="/games" @click='${(evt) => this.linkClick(evt, '/games')}'>Games</a>
       </li>
-     <li class="nav-item">
+     <li class="nav-item ${this.currentRoute === '/about' ? 'active' : 'empty'}">
         <a class="nav-link" href="/about" @click='${(evt) => this.linkClick(evt, '/about')}'>About</a>
       </li>
     </ul>
